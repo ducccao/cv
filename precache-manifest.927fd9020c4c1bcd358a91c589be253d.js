0a82512b3741fc2543dfd869df4044d8454d7f27
Trinh Cao Van Duc <44729657+PanAngelosk@users.noreply.github.com>
2020-08-19 11:30:42 +0700

    Updates

---
+++ precache-manifest.927fd9020c4c1bcd358a91c589be253d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55b1a06a6876afffe6a948fb84d667a8",
+    "url": "/Curriculum-vitae/index.html"
+  },
+  {
+    "revision": "ad284e958b8fbb14d687",
+    "url": "/Curriculum-vitae/static/css/main.f9b94475.chunk.css"
+  },
+  {
+    "revision": "3794e336efcbdc66b4b7",
+    "url": "/Curriculum-vitae/static/js/2.3150d39a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Curriculum-vitae/static/js/2.3150d39a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad284e958b8fbb14d687",
+    "url": "/Curriculum-vitae/static/js/main.4c160039.chunk.js"
+  },
+  {
+    "revision": "efdc8dd037c25b192228",
+    "url": "/Curriculum-vitae/static/js/runtime-main.461fe699.js"
+  },
+  {
+    "revision": "06468580303d77d9afd2801a2ca80086",
+    "url": "/Curriculum-vitae/static/media/fb.06468580.svg"
+  },
+  {
+    "revision": "3d96dbbeeccad41123c3f763d9d4b6f6",
+    "url": "/Curriculum-vitae/static/media/ins.3d96dbbe.svg"
+  }
+]);
